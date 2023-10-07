c35083ba352e9a8191bd3d80144e07f8d775c714
Abdul70057005 <abdyl2707@mail.ru>
2023-10-08 02:09:53 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  slidesPerView: 1.2,
+  spaceBetween: -10,
+});
+
+let clickButton = document.querySelector('.section-form__button');
+let textButton = document.querySelector('.section-form__label');
+let elemSectionUl = document.querySelector('.section-container');
+
+clickButton.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  if (textButton.textContent === 'Показать все') {
+    textButton.textContent = 'Скрыть';
+    elemSectionUl.classList.add('hidden');
+  } else {
+    textButton.textContent = 'Показать все';
+    elemSectionUl.classList.remove('hidden')
+  }
+ 
+});
